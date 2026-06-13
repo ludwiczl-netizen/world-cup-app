54661f38c5eb5a6d58293364313f8b040207072f
ludwiczl-netizen <ludwiczl@gmail.com>
2026-06-13 17:27:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+
 @app.get("/", response_class=HTMLResponse)
 def home():
     html = "<h2>Ranking</h2>"
     html += "<a href='/test'>Klik mnie</a>"
     return html
+
 
 @app.get("/test")
 def test():
