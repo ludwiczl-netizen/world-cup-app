4b9fed3227f75fcf68717e8de59efb9382eb7d1f
ludwiczl-netizen <ludwiczl@gmail.com>
2026-06-13 17:30:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,22 @@ from fastapi.responses import HTMLResponse
 app = FastAPI()
 
 
+
 @app.get("/", response_class=HTMLResponse)
 def home():
-    html = "<h2>Ranking</h2>"
-    html += "<a href='/test'>Klik mnie</a>"
+
+    html = "<h2>🏆 Ranking</h2>"
+    html += "<table border='1'>"
+    html += "<tr><th>#</th><th>Gracz</th><th>Pkt</th></tr>"
+
+    html += "<tr><td>1</td><td>Jan</td><td>10</td></tr>"
+    html += "<tr><td>2</td><td>Adam</td><td>8</td></tr>"
+
+    html += "</table>"
+    html += "<br><a href='/test'>Test</a>"
+
     return html
+
 
 
 @app.get("/test")
