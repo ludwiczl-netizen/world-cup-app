a2f0f60623d5e12c1e38022de503c2bfedbd1520
ludwiczl-netizen <ludwiczl@gmail.com>
2026-06-13 12:33:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 @app.get("/", response_class=HTMLResponse)
 def home():
     html = "<h2>Ranking</h2>"
-html += "<div onclick=\"location.href='/test
+html += "<div onclick=\"location.href='/test'ml
     return html
 
 @app.get("/test")
