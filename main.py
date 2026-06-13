382515325f95cd3309a2f9b1b60a15cb41df0901
ludwiczl-netizen <ludwiczl@gmail.com>
2026-06-13 17:48:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
-import pandas as pd
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+import pandas as pd
+import urllib.parse
+
 
 app = FastAPI()
 
 
 
+
+# ===== HOME (RANKING) =====
 @app.get("/", response_class=HTMLResponse)
 def home():
 
     df = pd.read_excel("tabela zbiorcza z rankingiem.xlsx")
+    df.columns = df.columns.str.strip()
 
     html = "<h2>🏆 Ranking</h2>"
     html += "<table border='1'>"
     html += "<tr><th>#</th><th>Gracz</th><th>Punkty</th></tr>"
 
     for i, row in df.iterrows():
+
+        name = str(row["Gracz"])
+        safe = urllib.parse.quote(name)
+
         html += "<tr>"
         html += "<td>" + str(i+1) + "</td>"
-        html += "<td>" + str(row["Gracz"]) + "</td>"
+        html += "<td> + "'>" + name + "</a></td>"
         html += "<td>" + str(row["Punkty"]) + "</td>"
         html += "</tr>"
 
@@ -26,6 +35,15 @@ def home():
 
     return html
 
-@app.get("/test")
-def test():
-    return "klik działa ✅"
+
+# ===== STRONA GRACZA =====
+@app.get("/gracz/{name}", response_class=HTMLResponse)
+def player(name: str):
+
+    name = urllib.parse.unquote(name)
+
+    html = "<h2>" + name + "</h2>"
+    html += "<br>Tutaj będą szczegóły gracza"
+    html += "<br>/⬅ Powrót</a>"
+
+    return html
